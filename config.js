50bb1c721eb8a4955efe0ec4ee8c5c0e955d0e92
ngNhut <ngNhut@unicorn365.net>
2022-10-21 00:40:07 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Uyen Lun - Tuyet Tuyet';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100069836841051';
+const eventName = "HAPPY WOMAN'S DAY!";
+const titleCard = 'Dear darling';
+const contentCard = 'Wishing you a whole lot of happiness, today and for the rest of the year. Every home, every heart, every feeling, every moment of happiness is incomplete without you, only you can complete this world. Happy Women’s Day!';
+
+const giftImage = 'a1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
